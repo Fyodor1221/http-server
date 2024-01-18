cc4bd1849cdc67056cfc26bdf969092d0f6968df
Fyodor Parfyonov <parfyonov.fyodor@gmail.com>
2024-01-18 23:50:01 +0300

    getting POST parameters added

--- src/main/java/ru/netology/server/Request.java
+++ src/main/java/ru/netology/server/Request.java
@@ -2,7 +2,9 @@ package ru.netology.server;
 
 import org.apache.http.NameValuePair;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Request {
@@ -34,10 +36,18 @@ public class Request {
     }
 
     public List<NameValuePair> getQueryParams() {
+        if (this.queryParams.size() < 1) {
+            System.out.println("There are no QUERY params");
+            return null;
+        }
         return queryParams;
     }
 
     public List<NameValuePair> getQueryParam(String name) {
+        if (this.queryParams.size() < 1) {
+            System.out.println("There are no QUERY params");
+            return null;
+        }
         var params = new ArrayList<NameValuePair>();
         for (NameValuePair param : queryParams) {
             if (param.getName().equals(name)) {
@@ -45,6 +55,30 @@ public class Request {
             }
         }
         return params;
+    }
+
+    public List<String> getPostParams() {
+        if (!this.getHeaders().contains("Content-Type: application/x-www-form-urlencoded")) {
+            System.out.println("Content-type must be \"application/x-www-form-urlencoded\"");
+            return null;
+        }
+        return Arrays.asList(this.getBody().split("&"));
+    }
+
+    public List<String> getPostParam(String name) throws IOException {
+        if (!this.getHeaders().contains("Content-Type: application/x-www-form-urlencoded")) {
+            System.out.println("Content-type must be \"application/x-www-form-urlencoded\"");
+            return null;
+        }
+        var postParam = new ArrayList<String>();
+        char[] value = new char[name.length()];
+        for (String param : this.getPostParams()) {
+            if (param.startsWith(name)) {
+                param.getChars(name.length() + 1, param.length(), value, 0);
+                postParam.add(new String(value).trim());
+            }
+        }
+        return postParam;
     }
 
     public void setMethod(String method) {
